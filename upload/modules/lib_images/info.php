cb1431645f8d6a327a3fde4d8d9bd8b2c75bbb1f
webbird <github.20.cerberus@spamgourmet.com>
2012-05-14 09:46:58 +0000

    moved class.Images.php from lib_lepton back to single module; added watermark patch (see issue #2 at GitHub, https://github.com/sprain/class.Images.php/issues/2)
    git-svn-id: https://server3.subversion-server.com/phpmanufaktur/svn/WebsiteBaker_Pro/branches/LEPTON_2_Special@1945 ac42e8e6-2193-fc42-897c-60656488c7e7

---
+++ upload/modules/lib_images/info.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * This file is part of an ADDON for use with LEPTON Core.
+ * This ADDON is released under the GNU GPL.
+ * Additional license terms can be seen in the info.php of this module.
+ *
+ * @module          class.Images.php
+ * @author          LEPTON Project
+ * @copyright       2012, LEPTON Project
+ * @link            http://www.lepton-cms.org
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @license_terms   please see info.php of this module
+ * @version         $Id$
+ *
+ */
+
+// try to include LEPTON class.secure.php to protect this file and the whole CMS!
+if (defined('WB_PATH')) {	
+	if (defined('LEPTON_VERSION')) include(WB_PATH.'/framework/class.secure.php');
+} elseif (file_exists($_SERVER['DOCUMENT_ROOT'].'/framework/class.secure.php')) {
+	include($_SERVER['DOCUMENT_ROOT'].'/framework/class.secure.php'); 
+} else {
+	$subs = explode('/', dirname($_SERVER['SCRIPT_NAME']));	$dir = $_SERVER['DOCUMENT_ROOT'];
+	$inc = false;
+	foreach ($subs as $sub) {
+		if (empty($sub)) continue; $dir .= '/'.$sub;
+		if (file_exists($dir.'/framework/class.secure.php')) { 
+			include($dir.'/framework/class.secure.php'); $inc = true;	break; 
+		} 
+	}
+	if (!$inc) trigger_error(sprintf("[ <b>%s</b> ] Can't include LEPTON class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+}
+// end include LEPTON class.secure.php
+
+$module_directory     = 'lib_images';
+$module_name          = 'class.Images.php for LEPTON';
+$module_function      = 'library';
+$module_version       = '0.2';
+$module_platform      = '2.x';
+$module_author        = 'class.Images.php - Manuel Reinhard, this module - Bianka Martinovic';
+$module_license       = 'GNU General Public License';
+$module_description   = 'class.Images.php - A class to handle and manipulate images';
+$module_home          = 'http://www.sprain.ch/';
+$module_guid          = 'C6A310F1-A270-4079-9EF3-977AA7EF380F';
+
+?>
---
+++ upload/modules/lib_images/uninstall.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of an ADDON for use with LEPTON Core.
+ * This ADDON is released under the GNU GPL.
+ * Additional license terms can be seen in the info.php of this module.
+ *
+ * @module          class.Images.php
+ * @author          LEPTON Project
+ * @copyright       2012, LEPTON Project
+ * @link            http://www.lepton-cms.org
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @license_terms   please see info.php of this module
+ * @version         $Id$
+ *
+ */
+
+
+// try to include LEPTON class.secure.php to protect this file and the whole CMS!
+if (defined('WB_PATH')) {	
+	if (defined('LEPTON_VERSION')) include(WB_PATH.'/framework/class.secure.php');
+} elseif (file_exists($_SERVER['DOCUMENT_ROOT'].'/framework/class.secure.php')) {
+	include($_SERVER['DOCUMENT_ROOT'].'/framework/class.secure.php'); 
+} else {
+	$subs = explode('/', dirname($_SERVER['SCRIPT_NAME']));	$dir = $_SERVER['DOCUMENT_ROOT'];
+	$inc = false;
+	foreach ($subs as $sub) {
+		if (empty($sub)) continue; $dir .= '/'.$sub;
+		if (file_exists($dir.'/framework/class.secure.php')) { 
+			include($dir.'/framework/class.secure.php'); $inc = true;	break; 
+		} 
+	}
+	if (!$inc) trigger_error(sprintf("[ <b>%s</b> ] Can't include LEPTON class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+}
+// end include LEPTON class.secure.php
+
+// Delete the lib directory
+rm_full_dir(WB_PATH.'/modules/lib_images');
+
+?>
