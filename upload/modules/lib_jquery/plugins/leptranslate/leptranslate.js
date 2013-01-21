15f8c3ebb03ef3e29aad5c492f43ba7728d41a8d
Bianka Martinovic <info@webbird.de>
2013-01-21 16:47:39 +0100

    updated leptranslate jQuery plugin

--- upload/modules/lib_jquery/plugins/leptranslate/leptranslate.js
+++ upload/modules/lib_jquery/plugins/leptranslate/leptranslate.js
@@ -1,11 +1,19 @@
-var url = 'http://localhost/_projects/Lepton2/modules/lib_jquery/plugins/leptranslate/leptranslate.php';
+/**
+ *   @author          LEPTON v2.0 Black Cat Edition Development
+ *   @copyright       2013, LEPTON v2.0 Black Cat Edition Development
+ *   @link            http://www.lepton2.org
+ *   @license         http://www.gnu.org/licenses/gpl.html
+ *   @category        LEPTON2BCE_Modules
+ *   @package         lib_jquery
+ *
+ */
+
+var url = LEPTON_URL + '/modules/lib_jquery/plugins/leptranslate/leptranslate.php';
 
 if ( typeof jQuery == 'undefined' ) {
   alert( 'FATAL ERROR! jQuery not available!' );
 }
-else {
-
-  // ----- AJAX Setup -----
+else {  // ----- AJAX Setup -----
   jQuery.ajaxSetup({
     error: function( x, e ){
       if( x.status == 0 )           { alert('You are offline!!\n Please Check Your Network.'); }
@@ -16,17 +24,17 @@ else {
       else                          { alert('Unknown Error.\n'+x.responseText);                }
     }
   });
-
-	function leptranslate( string, elem, attributes ) {
+	function leptranslate( string, elem, attributes, module ) {
       jQuery.post(
 		url,
 		{
 	      msg:  string,
-	      attr: attributes
+	      attr: attributes,
+          mod: module
 	    },
 		function( data ) {
           if ( typeof elem != 'undefined' && typeof elem != '' ) {
-		      jQuery(elem).append(jQuery(data).text());
+		      jQuery(elem).text(jQuery(data).text());
           }
           else {
               return jQuery(data).text();
--- upload/modules/lib_jquery/plugins/leptranslate/leptranslate.php
+++ upload/modules/lib_jquery/plugins/leptranslate/leptranslate.php
@@ -1,26 +1,34 @@
 <?php
 
 /**
- * This file is part of an ADDON for use with LEPTON Core.
- * This ADDON is released under the GNU GPL.
- * Additional license terms can be seen in the info.php of this module.
- *
- *
- * @author          LEPTON Project
- * @copyright       2010-2011, LEPTON Project
- * @link            http://www.LEPTON-cms.org
+ *   @author          LEPTON v2.0 Black Cat Edition Development
+ *   @copyright       2013, LEPTON v2.0 Black Cat Edition Development
+ *   @link            http://www.lepton2.org
  * @license         http://www.gnu.org/licenses/gpl.html
- * @license_terms   please see info.php of this module
- * @version         $Id: index.php 1374 2011-11-15 11:22:48Z creativecat $
+ *   @category        LEPTON2BCE_Modules
+ *   @package         lib_jquery
  *
  */
 
+@include dirname(__FILE__).'/../../../../config.php';
 @include dirname(__FILE__).'/../../../../framework/LEPTON/Helper/I18n.php';
+@include dirname(__FILE__).'/../../../../framework/LEPTON/Helper/Directory.php';
 $lang = new LEPTON_Helper_I18n();
 $attr = ( isset($_POST['attr']) ? $_POST['attr'] : NULL );
 
+if ( isset($_POST['mod']) ) {
+    $mod  = $_POST['mod'];
+    $d    = new LEPTON_Helper_Directory();
+    $path = $d->sanitizePath(dirname(__FILE__).'/../../../../modules/'.$mod);
+    if( is_dir($path) ) {
+        if( file_exists($path.'/languages/'.$lang->getLang().'.php') ) {
+            $lang->addFile( $lang->getLang().'.php', $path.'/languages/' );
+        }
+    }
+}
+
 if ( is_object($lang) ) {
-	echo '<data>'.$lang->translate( $_POST['msg'] ).'</data>';
+	echo '<data>'.$lang->translate( $_POST['msg'], $attr ).'</data>';
 }
 else {
 	echo "Error<br />";
