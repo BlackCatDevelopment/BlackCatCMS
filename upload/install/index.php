2973eea49bc9da772f67f6d27f6f85f7b872d3e0
Bianka Martinovic <info@webbird.de>
2013-02-22 19:20:15 +0100

    fixed installer; fixed news install.php

--- upload/install/index.php
+++ upload/install/index.php
@@ -611,6 +611,14 @@ function show_step_finish() {
 	{
 	    $tpl = 'finish_'.$lang->getLang().'.lte';
 	}
+	// fix globals
+	$parser->setGlobals(
+    	array(
+    	    'installer_uri' => $installer_uri,
+    	)
+    );
+	// fix path (some modules may change it)
+	$parser->setPath( dirname(__FILE__).'/templates/default' );
 	$output = $parser->get(
 		$tpl,
 		array(
@@ -1141,13 +1149,11 @@ function __cat_installer_import_sql($file,$database) {
     $import = preg_replace( "%^--(.*)\n%mU" , ''          , $import );
     $import = preg_replace( "%^$\n%mU"      , ''          , $import );
     $import = preg_replace( "%lep_%"        , CAT_TABLE_PREFIX, $import );
-    $import = preg_replace( "%\r?\n%"       , ''          , $import );
-
-    $import = explode (";", $import);
-
-    foreach ($import as $imp){
-        if ($imp != '' && $imp != ' '){
+    
+    foreach (split_sql_file($import, ';') as $imp){
+        if ($imp != '' && $imp != ' ') {
             $ret = $database->query($imp);
+//echo $database->get_error(), "<br />";
         }
     }
 
@@ -1284,7 +1290,6 @@ function __do_install() {
 			}
 			// only try to install modules if fill tables succeeded
 			else {
-echo "installing modules<br />";
 				// ----- install addons -----
 				list ( $result, $insterrors ) = install_modules($cat_path);
 				if ( ! $result || count($insterrors) ) {
@@ -1447,3 +1452,103 @@ function __cat_check_db_config() {
 	return $errors;
 
 }   // end function __cat_check_db_config()
+
+/**
+ * Credits: http://stackoverflow.com/questions/147821/loading-sql-files-from-within-php
+ **/
+function split_sql_file($sql, $delimiter)
+{
+   // Split up our string into "possible" SQL statements.
+   $tokens = explode($delimiter, $sql);
+
+   // try to save mem.
+   $sql = "";
+   $output = array();
+
+   // we don't actually care about the matches preg gives us.
+   $matches = array();
+
+   // this is faster than calling count($oktens) every time thru the loop.
+   $token_count = count($tokens);
+   for ($i = 0; $i < $token_count; $i++)
+   {
+      // Don't wanna add an empty string as the last thing in the array.
+      if (($i != ($token_count - 1)) || (strlen($tokens[$i] > 0)))
+      {
+         // This is the total number of single quotes in the token.
+         $total_quotes = preg_match_all("/'/", $tokens[$i], $matches);
+         // Counts single quotes that are preceded by an odd number of backslashes,
+         // which means they're escaped quotes.
+         $escaped_quotes = preg_match_all("/(?<!\\\\)(\\\\\\\\)*\\\\'/", $tokens[$i], $matches);
+
+         $unescaped_quotes = $total_quotes - $escaped_quotes;
+
+         // If the number of unescaped quotes is even, then the delimiter did NOT occur inside a string literal.
+         if (($unescaped_quotes % 2) == 0)
+         {
+            // It's a complete sql statement.
+            $output[] = $tokens[$i];
+            // save memory.
+            $tokens[$i] = "";
+         }
+         else
+         {
+            // incomplete sql statement. keep adding tokens until we have a complete one.
+            // $temp will hold what we have so far.
+            $temp = $tokens[$i] . $delimiter;
+            // save memory..
+            $tokens[$i] = "";
+
+            // Do we have a complete statement yet?
+            $complete_stmt = false;
+
+            for ($j = $i + 1; (!$complete_stmt && ($j < $token_count)); $j++)
+            {
+               // This is the total number of single quotes in the token.
+               $total_quotes = preg_match_all("/'/", $tokens[$j], $matches);
+               // Counts single quotes that are preceded by an odd number of backslashes,
+               // which means they're escaped quotes.
+               $escaped_quotes = preg_match_all("/(?<!\\\\)(\\\\\\\\)*\\\\'/", $tokens[$j], $matches);
+
+               $unescaped_quotes = $total_quotes - $escaped_quotes;
+
+               if (($unescaped_quotes % 2) == 1)
+               {
+                  // odd number of unescaped quotes. In combination with the previous incomplete
+                  // statement(s), we now have a complete statement. (2 odds always make an even)
+                  $output[] = $temp . $tokens[$j];
+
+                  // save memory.
+                  $tokens[$j] = "";
+                  $temp = "";
+
+                  // exit the loop.
+                  $complete_stmt = true;
+                  // make sure the outer loop continues at the right point.
+                  $i = $j;
+               }
+               else
+               {
+                  // even number of unescaped quotes. We still don't have a complete statement.
+                  // (1 odd and 1 even always make an odd)
+                  $temp .= $tokens[$j] . $delimiter;
+                  // save memory.
+                  $tokens[$j] = "";
+               }
+
+            } // for..
+         } // else
+      }
+   }
+   
+   // remove empty
+   for ( $i = count($output)+1; $i>=0; $i-- )
+   {
+       if ( trim($output[$i]) == '' )
+       {
+           array_splice($output, $i, 1);
+       }
+   }
+
+   return $output;
+}
--- upload/modules/news/install.php
+++ upload/modules/news/install.php
@@ -1,36 +1,54 @@
 <?php
 
 /**
- *  @module         news
- *  @version        see info.php of this module
- *  @author         Ryan Djurovich, Rob Smith, Dietrich Roland Pehlke, Christian M. Stefan (Stefek), Jurgen Nijhuis (Argos)
- *  @copyright      2004-2011, Ryan Djurovich, Rob Smith, Dietrich Roland Pehlke, Christian M. Stefan (Stefek), Jurgen Nijhuis (Argos) 
- *  @license        GNU General Public License
- *  @license terms  see info.php of this module
- *  @platform       see info.php of this module
- *  @requirements   PHP 5.2.x and higher
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or (at
+ *   your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *   General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ *   @author          Website Baker Project, LEPTON Project, Black Cat Development
+ *   @copyright       2004-2010, Website Baker Project
+ *   @copyright       2011-2012, LEPTON Project
+ *   @copyright       2013, Black Cat Development
+ *   @link            http://www.blackcat-cms.org
+ *   @license         http://www.gnu.org/licenses/gpl.html
+ *   @category        CAT_Module
+ *   @package         news
+ *
  */
 
 // include class.secure.php to protect this file and the whole CMS!
-if (defined('CAT_PATH')) {	
-	include(CAT_PATH.'/framework/class.secure.php'); 
-} else {
-	$oneback = "../";
-	$root = $oneback;
-	$level = 1;
-	while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
-		$root .= $oneback;
-		$level += 1;
-	}
-	if (file_exists($root.'/framework/class.secure.php')) { 
-		include($root.'/framework/class.secure.php'); 
-	} else {
-		trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
-	}
+if ( defined( 'CAT_PATH' ) )
+{
+    include( CAT_PATH . '/framework/class.secure.php' );
 }
-// end include class.secure.php
-
-
+else
+{
+    $oneback = "../";
+    $root    = $oneback;
+    $level = 1;
+    while ( ( $level < 10 ) && ( !file_exists( $root . '/framework/class.secure.php' ) ) )
+    {
+        $root .= $oneback;
+        $level += 1;
+    }
+    if ( file_exists( $root . '/framework/class.secure.php' ) )
+    {
+        include( $root . '/framework/class.secure.php' );
+    }
+    else
+    {
+        trigger_error( sprintf( "[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER[ 'SCRIPT_NAME' ] ), E_USER_ERROR );
+    }
+}
 
 if(defined('CAT_URL'))
 {
@@ -188,7 +206,7 @@ header('Location: ../');
 		$handle = fopen(CAT_PATH.PAGES_DIRECTORY.'/posts/index.php', 'w');
 		fwrite($handle, $content);
 		fclose($handle);
-		change_mode(CAT_PATH.PAGES_DIRECTORY.'/posts/index.php', 'file');
+		CAT_Helper_Directory::getInstance()->setPerms(CAT_PATH.PAGES_DIRECTORY.'/posts/index.php');
 		
 		/**
 		 *	Try to copy the index.php also in the newspicts folder inside
