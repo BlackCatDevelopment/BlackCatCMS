c05bf17315c006bd7f1998c9417ad6269bd6e4ae
Bianka Martinovic <info@webbird.de>
2013-03-01 13:00:09 +0100

    moved login methods to CAT_Users.php

--- upload/backend/login/ajax_index.php
+++ upload/backend/login/ajax_index.php
@@ -1,66 +1,69 @@
 <?php
 
 /**
- * This file is part of LEPTON2 Core, released under the GNU GPL
- * Please see LICENSE and COPYING files in your package for details, specially for terms and warranties.
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or (at
+ *   your option) any later version.
  * 
- * NOTICE:LEPTON CMS Package has several different licenses.
- * Please see the individual license in the header of each single file or info.php of modules and templates.
+ *   This program is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *   General Public License for more details.
  *
- * @author			LEPTON2 Project
- * @copyright		2012, LEPTON2 Project
- * @link			http://lepton2.org
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ *   @author          Black Cat Development
+ *   @copyright       2013, Black Cat Development
+ *   @link            http://blackcat-cms.org
  * @license			http://www.gnu.org/licenses/gpl.html
- * @license_terms	please see LICENSE and COPYING files in your package
- *
+ *   @category        CAT_Core
+ *   @package         CAT_Core
  *
  */
  
-if(!defined('CAT_PATH')) require dirname(__FILE__) . '/../../config.php';
+define('CAT_LOGIN_PHASE',1);
 
-require_once( CAT_PATH . '/framework/class.login.php');
+if (defined('CAT_PATH')) {
+	include(CAT_PATH.'/framework/class.secure.php');
+} else {
+	$oneback = "../";
+	$root = $oneback;
+	$level = 1;
+	while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
+		$root .= $oneback;
+		$level += 1;
+	}
+	if (file_exists($root.'/framework/class.secure.php')) {
+		include($root.'/framework/class.secure.php');
+	} else {
+		trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+	}
+}
 
-$salt = md5(microtime());
-
-// ================================================ 
-// ! we want difference hashes for the two fields   
-// ================================================ 
-$username_fieldname		= 'username_' . substr($salt, 0, 7);
-$password_fieldname		= 'password_' . substr($salt, -7);
-
-$thisApp = new Login(
-	array(
-		'MAX_ATTEMPTS'			=> MAX_ATTEMPTS,
-		'WARNING_URL'			=> CAT_THEME_URL . '/templates/warning.html',
-		'USERNAME_FIELDNAME'	=> $username_fieldname,
-		'PASSWORD_FIELDNAME'	=> $password_fieldname,
-		'MIN_USERNAME_LEN'		=> AUTH_MIN_LOGIN_LENGTH,
-		'MAX_USERNAME_LEN'		=> AUTH_MAX_LOGIN_LENGTH,
-		'MIN_PASSWORD_LEN'		=> AUTH_MIN_PASS_LENGTH,
-		'MAX_PASSWORD_LEN'		=> AUTH_MAX_PASS_LENGTH,
-		'LOGIN_URL'				=> CAT_ADMIN_URL . '/login/index.php',
-		'DEFAULT_URL'			=> CAT_ADMIN_URL . '/start/index.php',
-		'TEMPLATE_DIR'			=> CAT_THEME_URL . '/templates',
-		'TEMPLATE_FILE'			=> 'login.lte',
-		'FRONTEND'				=> false,
-		'REDIRECT_URL'			=> CAT_ADMIN_URL . '/start/index.php',
-		'FORGOTTEN_DETAILS_APP'	=> CAT_ADMIN_URL . '/login/forgot/index.php',
-		'USERS_TABLE'			=> CAT_TABLE_PREFIX . 'users',
-		'GROUPS_TABLE'			=> CAT_TABLE_PREFIX . 'groups',
-		'OUTPUT'				=> false,
-		'PAGE_ID'				=> ''
-	),
-	false
-);
+if ( CAT_Helper_Validate::getInstance()->fromSession('ATTEMPTS') > MAX_ATTEMPTS )
+{
+    $redirect = THEME_URL . '/templates/warning.html';
+    $ajax	= array(
+    	'url'			=> $redirect,
+    	'success'		=> true,
+    	'message'		=> NULL
+    );
+}
+else
+{
+    #CAT_Helper_Protect::getInstance()->enableCSRFMagic();
+    $redirect = CAT_Users::getInstance()->handleLogin();
+    $error    = CAT_Users::getInstance()->loginError();
+    $ajax	= array(
+    	'url'			=> $redirect,
+    	'success'		=> CAT_Users::getInstance()->is_authenticated(),
+    	'message'		=> ( ($redirect===false||$error) ? $error : NULL ),
+    );
+}
 
 header('Content-type: application/json');
-$ajax	= array(
-	'ctoken'		=> $thisApp->is_authenticated() ? $thisApp->getToken() : false,
-	'url'			=> $thisApp->url,
-	'success'		=> $thisApp->is_authenticated(),
-	'message'		=> isset($thisApp->message) ? $thisApp->message : false
-);
-
 print json_encode( $ajax );
 exit();
 
--- upload/backend/login/index.php
+++ upload/backend/login/index.php
@@ -23,6 +23,8 @@
  *
  */
  
+define('CAT_LOGIN_PHASE',1);
+
 if (defined('CAT_PATH')) {	
 	include(CAT_PATH.'/framework/class.secure.php'); 
 } else {
@@ -40,43 +42,5 @@ if (defined('CAT_PATH')) {
 	}
 }
 
-#if(empty($_SERVER["HTTPS"]) || $_SERVER["HTTPS"] !== "on")
-#{
-#    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-#    exit();
-#}
-
-require_once(CAT_PATH . "/framework/class.login.php");
-
-$salt = md5(microtime());
-// ================================================ 
-// ! we want difference hashes for the two fields   
-// ================================================ 
-$username_fieldname		= 'username_'.substr($salt, 0, 7);
-$password_fieldname		= 'password_'.substr($salt, -7);
-
-$thisApp = new Login(
-	array(
-	'MAX_ATTEMPTS'			=> MAX_ATTEMPTS,
-	'WARNING_URL'			=> CAT_THEME_URL . '/templates/warning.html',
-	'USERNAME_FIELDNAME'	=> $username_fieldname,
-	'PASSWORD_FIELDNAME'	=> $password_fieldname,
-	'MIN_USERNAME_LEN'		=> AUTH_MIN_LOGIN_LENGTH,
-	'MAX_USERNAME_LEN'		=> AUTH_MAX_LOGIN_LENGTH,
-	'MIN_PASSWORD_LEN'		=> AUTH_MIN_PASS_LENGTH,
-	'MAX_PASSWORD_LEN'		=> AUTH_MAX_PASS_LENGTH,
-	'LOGIN_URL'				=> CAT_ADMIN_URL.'/login/index.php',
-	'DEFAULT_URL'			=> CAT_ADMIN_URL.'/start/index.php',
-	'TEMPLATE_DIR'			=> CAT_THEME_PATH.'/templates',
-	'TEMPLATE_FILE'			=> 'login.lte',
-	'FRONTEND'				=> false,
-	'REDIRECT_URL'			=> CAT_ADMIN_URL . '/start/index.php',
-	'FORGOTTEN_DETAILS_APP'	=> CAT_ADMIN_URL . '/login/forgot/index.php',
-	'USERS_TABLE'			=> CAT_TABLE_PREFIX . 'users',
-	'GROUPS_TABLE'			=> CAT_TABLE_PREFIX . 'groups',
-	'OUTPUT'				=> true,
-	'PAGE_ID'				=> '',
-	true,
-	)
-);
-?>
+CAT_Helper_Protect::getInstance()->enableCSRFMagic();
+CAT_Users::getInstance()->handleLogin();
