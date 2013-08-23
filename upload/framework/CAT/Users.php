d141dc358da3c86bc326a4081eb941ec2246c05d
Bianka Martinovic <info@webbird.de>
2013-08-23 15:49:44 +0200

    added class for password complexity checks to CAT_Users

--- upload/framework/CAT/Users.php
+++ upload/framework/CAT/Users.php
@@ -32,14 +32,6 @@ if ( ! class_exists( 'CAT_Users', false ) )
 	class CAT_Users extends CAT_Object
 	{
 	
-	    // Checking password complexity
-		// This regular expression will tests if the input consists of 6 or more
-		// letters, digits, underscores and hyphens.
-		// The input must contain at least one upper case letter, one lower case
-		// letter and one digit.
-        private static $PCRE_PASSWORD
-            = "/^\A(?=[\.,;\:&\"\'\?\!\(\)a-zA-Z0-9]*?[A-Z])(?=[\.,;\:&\"\'\?\!\(\)a-zA-Z0-9]*?[a-z])(?=[\.,;\:&\"\'\?\!\(\)a-zA-Z0-9]*?[0-9])\S{6,}\z$/";
-		
         private static $validatePasswordError = NULL;
         private static $lastValidatedPassword = NULL;
         private static $loginerror            = false;
@@ -1183,7 +1175,8 @@ if ( ! class_exists( 'CAT_Users', false ) )
             $max_length = CAT_Registry::exists('AUTH_MAX_PASS_LENGTH')
                         ? CAT_Registry::get('AUTH_MAX_PASS_LENGTH')
                         : 20;
-	        // check length
+
+	        // ----- check length -----
 	        if (
                    strlen($password) < $min_length
                 && (
@@ -1221,9 +1214,12 @@ if ( ! class_exists( 'CAT_Users', false ) )
 	        // check complexity
 	        if ( $strict )
 	        {
-                if ( ! preg_match( self::$PCRE_PASSWORD, $password ) )
+                $PASSWORD = new Password();
+                $PASSWORD->setComplexity($PASSWORD->getComplexityStrict());
+                if (!$PASSWORD->complexEnough($password, self::get_username()))
 	            {
-                    self::$validatePasswordError = self::lang()->translate('The required password complexity is not met');
+                    self::$validatePasswordError = self::lang()->translate('The required password complexity is not met').
+                    implode('<br />',$PASSWORD->getPasswordIssues());
 					return false;
 	            }
 	        }
@@ -1305,5 +1301,128 @@ if ( ! class_exists( 'CAT_Users', false ) )
 	}
 
 }
+
+/*******************************************************************************
+ * http://aaronsaray.com/blog/2009/02/12/password-complexity-class/
+ ******************************************************************************/
+class Password
+{
+	/** constants - are arbritrary numbers - but used for bitwise **/
+	const REQUIRE_LOWERCASE       = 4;
+	const REQUIRE_UPPERCASE       = 8;
+	const REQUIRE_NUMBER	      = 16;
+	const REQUIRE_SPECIALCHAR     = 32;
+	//const REQUIRE_DIFFPASS	      = 64;
+	const REQUIRE_DIFFUSER	      = 128;
+	const REQUIRE_UNIQUE	      = 256;
+	protected $_passwordDiffLevel = 3;
+	protected $_uniqueChrRequired = 4;
+	protected $_complexityLevel   = 0;
+	protected $_issues            = array();
+	/**
+	 * returns the standard options
+	 * @return integer
+	 */
+	public function getComplexityStandard()
+	{
+		return self::REQUIRE_LOWERCASE + self::REQUIRE_UPPERCASE + self::REQUIRE_NUMBER;
+	}
+	/**
+	 *returns all of the options
+	 *@return integer
+	 */
+	public function getComplexityStrict()
+	{
+		$r = new ReflectionClass($this);
+		$complexity = 0;
+		foreach ($r->getConstants() as $constant) {
+			$complexity += $constant;
+		}
+		return $complexity;
+	}
+	public function setComplexity($complexityLevel)
+	{
+		$this->_complexityLevel=$complexityLevel;
+	}
+	/**
+	 * checks for complexity level. If returns false, it has populated the _issues array
+	 */
+	public function complexEnough($newPass, $username, $oldPass = NULL)
+	{
+		$enough = TRUE;
+		$r      = new ReflectionClass($this);
+		foreach ($r->getConstants() as $name=>$constant) {
+			/** means we have to check that type then **/
+			if ($this->_complexityLevel & $constant) {
+				/** REQUIRE_MIN becomes _requireMin() **/
+				$parts    = explode('_', $name, 2);
+				$funcName = "_{$parts[0]}" . ucwords($parts[1]);
+				$result   = call_user_func_array(array($this, $funcName), array($newPass, $oldPass, $username));
+				if ($result !== TRUE) {
+					$enough = FALSE;
+					$this->_issues[] = $result;
+				}
+			}
+		}
+		return $enough;
+	}
+	public function getPasswordIssues()
+	{
+		return $this->_issues;
+	}
+	protected function _requireLowercase($newPass)
+	{
+		if (!preg_match('/[a-z]/', $newPass)) {
+			return 'Password requires a lowercase letter.';
+		}
+		return true;
+	}
+	protected function _requireUppercase($newPass)
+	{
+		if (!preg_match('/[A-Z]/', $newPass)) {
+			return 'Password requires an uppercase letter.';
+		}
+		return true;
+	}
+	protected function _requireNumber($newPass)
+	{
+		if (!preg_match('/[0-9]/', $newPass)) {
+			return 'Password requires a number.';
+		}
+		return true;
+	}
+	protected function _requireSpecialChar($newPass)
+	{
+		if (!preg_match('/[^a-zA-Z0-9]/', $newPass)) {
+			return 'Password requires a special character.';
+		}
+		return true;
+	}
+	protected function _requireDiffpass($newPass, $oldPass)
+	{
+		if (strlen($newPass) - similar_text($oldPass,$newPass) < $this->_passwordDiffLevel || stripos($newPass, $oldPass) !== FALSE) {
+			return 'Password must be a bit more different than the last password.';
+		}
+		return true;
+	}
+	protected function _requireDiffuser($newPass, $oldPass, $username)
+	{
+		if (stripos($newPass, $username) !== FALSE) {
+			return 'Password should not contain your username.';
+		}
+		return true;
+	}
+	protected function _requireUnique($newPass)
+	{
+		$uniques = array_unique(str_split($newPass));
+		if (count($uniques) < $this->_uniqueChrRequired) {
+			return 'Password must contain more unique characters.';
+		}
+		return true;
+	}
+}
+
+
+
 
 ?>
