2f38a957e7c43fab4c1e931bb65cb8f849baa4ab
Bianka Martinovic <info@webbird.de>
2013-11-05 20:21:50 +0100

    rewritten search.php in wysiwyg module

--- upload/modules/wysiwyg/search.php
+++ upload/modules/wysiwyg/search.php
@@ -1,25 +1,30 @@
 <?php
 
 /**
- * This file is part of an ADDON for use with Black Cat CMS Core.
- * This ADDON is released under the GNU GPL.
- * Additional license terms can be seen in the info.php of this module.
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or (at
+ *   your option) any later version.
  *
- * @module          wysiwyg
- * @author          Ryan Djurovich
- * @author          LEPTON Project
- * @copyright       2004-2010 WebsiteBaker Project
- * @copyright       2010-2011 LEPTON Project 
- * @link            http://www.LEPTON-cms.org
- * @license         http://www.gnu.org/licenses/gpl.html
- * @license_terms   please see info.php of this module
+ *   This program is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *   General Public License for more details.
  *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ *   @author          Black Cat Development
+ *   @copyright       2013, Black Cat Development
+ *   @link            http://blackcat-cms.org
+ *   @license         http://www.gnu.org/licenses/gpl.html
+ *   @category        CAT_Core
+ *   @package         CAT_Core
  *
  */
 
-// include class.secure.php to protect this file and the whole CMS!
-if (defined('CAT_PATH')) {	
-	include(CAT_PATH.'/framework/class.secure.php'); 
+if (defined('CAT_PATH')) {
+	include(CAT_PATH.'/framework/class.secure.php');
 } else {
 	$root = "../";
 	$level = 1;
@@ -27,51 +32,43 @@ if (defined('CAT_PATH')) {
 		$root .= "../";
 		$level += 1;
 	}
-	if (file_exists($root.'/framework/class.secure.php')) { 
-		include($root.'/framework/class.secure.php'); 
+	if (file_exists($root.'/framework/class.secure.php')) {
+		include($root.'/framework/class.secure.php');
 	} else {
 		trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
 	}
 }
-// end include class.secure.php
 
- 
-
-function wysiwyg_search($func_vars) {
+function wysiwyg_search($func_vars)
+{
 	extract($func_vars, EXTR_PREFIX_ALL, 'func');
-	static $search_sql = FALSE;
-	if(function_exists('search_make_sql_part')) {
-		if($search_sql===FALSE)
-			$search_sql = search_make_sql_part($func_search_url_array, $func_search_match, array('`content`'));
-	} else {
-		$search_sql = '1=1';
-	}  
 	
 	// how many lines of excerpt we want to have at most
 	$max_excerpt_num = $func_default_max_excerpt;
-	$divider = ".";
-	$result = false;
+	$divider         = ".";
+	$result          = false;
 	
-	// we have to get 'content' instead of 'text', because strip_tags() dosen't remove scripting well.
+	// we have to get 'content' instead of 'text', because strip_tags()
+    // doesn't remove scripting well.
 	// scripting will be removed later on automatically
-	$table = CAT_TABLE_PREFIX."mod_wysiwyg";
-	$query = $func_database->query("
-		SELECT content
-		FROM $table
-		WHERE section_id='$func_section_id'
-	");
+	$query = $func_database->query(sprintf(
+        "SELECT content FROM `%smod_wysiwyg` WHERE section_id='%d'",
+        CAT_TABLE_PREFIX, $func_section_id
+	));
 
-	if($query->numRows() > 0) {
-		if($res = $query->fetchRow()) {
+	if($query->numRows() > 0)
+    {
+		if($res = $query->fetchRow())
+        {
 			$mod_vars = array(
-				'page_link' => $func_page_link,
-				'page_link_target' => "#wb_section_$func_section_id",
-				'page_title' => $func_page_title,
-				'page_description' => $func_page_description,
+				'page_link'          => $func_page_link,
+				'page_link_target'   => SEC_ANCHOR."#section_$func_section_id",
+				'page_title'         => $func_page_title,
+				'page_description'   => $func_page_description,
 				'page_modified_when' => $func_page_modified_when,
-				'page_modified_by' => $func_page_modified_by,
-				'text' => $res['content'].$divider,
-				'max_excerpt_num' => $max_excerpt_num
+				'page_modified_by'   => $func_page_modified_by,
+				'text'               => $res['content'].$divider,
+				'max_excerpt_num'    => $max_excerpt_num
 			);
 			if(print_excerpt2($mod_vars, $func_vars)) {
 				$result = true;
