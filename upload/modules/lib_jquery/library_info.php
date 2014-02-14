6b534ab93f1c0dde47208d3c605493fc0de0a2f2
Bianka Martinovic <info@webbird.de>
2014-02-14 12:01:43 +0100

    upgraded jquery core to v1.11, UI to 1.10.4, removed duplicate cookie plugin

--- upload/modules/lib_jquery/library_info.php
+++ upload/modules/lib_jquery/library_info.php
@@ -2,7 +2,7 @@
 
 /*******************************************************************************
     Library Admin - jQuery Library
-    (c) 2010 - 2012 Bianka Martinovic - All rights reserved
+    (c) 2010 - 2013 Bianka Martinovic - All rights reserved
     http://www.webbird.de/
 *******************************************************************************/
 
@@ -10,8 +10,8 @@ global $lib_info;
 $lib_info = array(
     'library_path'    => 'lib_jquery',
     'library_name'    => 'jQuery',
-    'library_version' => '1.8.4',
-    'library_info'    => 'Includes jQuery Core v1.10.2, jQuery UI 1.10.3, UI Theme "base"',
+    'library_version' => '1.11.0',
+    'library_info'    => 'Includes jQuery Core v1.11.0, jQuery UI 1.10.4, UI Theme "base"',
     'library_class'   => 'jQueryAdmin',
     'preset_suffix'   => 'jquery',
     'scan_dirs'       => array(
