2458d57401cac07cad495244219ecbd85211832b
Matthias Glienke <support@creativecat.de>
2015-02-20 01:50:22 +0100

    Enhancement on #281

--- upload/framework/CAT/Helper/Page.php
+++ upload/framework/CAT/Helper/Page.php
@@ -1388,10 +1388,10 @@ frontend.css and template.css are added in _get_css()
             // page title
             if(isset($droplets_config['page_title']))
                 $title = $droplets_config['page_title'];
+            elseif(defined('WEBSITE_TITLE'))
+                $title = WEBSITE_TITLE . (isset($properties['page_title']) ? ' - ' . $properties['page_title'] : '' );
             elseif(isset($properties['page_title']))
                 $title = $properties['page_title'];
-            elseif(defined('WEBSITE_TITLE'))
-                $title = WEBSITE_TITLE;
             else
                 $title = '-';
             if($title)
