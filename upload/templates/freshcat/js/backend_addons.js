45a86353c94d64a9eec07ae8c23d184aa967b5ac
Bianka Martinovic <info@webbird.de>
2018-01-08 13:10:43 +0100

    fix: "show not installed modules" button not working in backend

--- upload/templates/freshcat/js/backend_addons.js
+++ upload/templates/freshcat/js/backend_addons.js
@@ -1,6 +1,6 @@
 /*
        2014, Black Cat Development
-   @link            https://blackcat-cms.org
+   @link            http://blackcat-cms.org
    @license         http://www.gnu.org/licenses/gpl.html
    @category        CAT_Core
    @package         freshcat
