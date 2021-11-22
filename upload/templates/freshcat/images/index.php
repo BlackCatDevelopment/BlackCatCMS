5d489ee33afb9f9fb8f1c21c19530f89cba61776
creativecat <support@creativecat.de>
2021-11-22 23:57:30 +0100

    class.secure.php is now included correctly

--- upload/templates/freshcat/images/index.php
+++ upload/templates/freshcat/images/index.php
@@ -30,12 +30,12 @@ if (defined('CAT_PATH')) {
 } else {
 	$root = "../";
 	$level = 1;
-	while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
+	while (($level < 10) && (!file_exists($root.'framework/class.secure.php'))) {
 		$root .= "../";
 		$level += 1;
 	}
-	if (file_exists($root.'/framework/class.secure.php')) { 
-		include($root.'/framework/class.secure.php'); 
+	if (file_exists($root.'framework/class.secure.php')) { 
+		include($root.'framework/class.secure.php'); 
 	} else {
 		trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
 	}
