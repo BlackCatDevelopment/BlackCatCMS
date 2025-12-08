55afd0da1de764d669b3ab74b5e440fd8f2b66db
creativecat <support@creativecat.de>
2025-12-08 18:20:17 +0100

    Update Dwoo

--- upload/modules/lib_dwoo/dwoo/dwooAutoload.php
+++ upload/modules/lib_dwoo/dwoo/dwooAutoload.php
@@ -1,18 +1,12 @@
 <?php
 
-require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'Dwoo' . DIRECTORY_SEPARATOR . 'Core.php';
-defined('DWOO_DIRECTORY') || define('DWOO_DIRECTORY',dirname(__FILE__) . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR);
+include dirname(__FILE__) . DIRECTORY_SEPARATOR . 'Dwoo.php';
 
-if(!function_exists('dwooAutoload')) {
-    function dwooAutoload($class)
-    {
-    	if (substr($class, 0, 5) === 'Dwoo\\') {
-            $file = DWOO_DIRECTORY . strtr($class, '\\', DIRECTORY_SEPARATOR).'.php';
-            if(file_exists($file)) {
-    		    include $file;
-            }
-    	}
-    }
-
-    spl_autoload_register('dwooAutoload');
+function dwooAutoload($class)
+{
+	if (substr($class, 0, 5) === 'Dwoo_') {
+		include DWOO_DIRECTORY . strtr($class, '_', DIRECTORY_SEPARATOR).'.php';
+	}
 }
+
+spl_autoload_register('dwooAutoload');
--- upload/modules/lib_dwoo/info.php
+++ upload/modules/lib_dwoo/info.php
@@ -26,13 +26,13 @@
 
 /**
  * #################### DWOO PHP5 TEMPLATE ENGINE ####################
- *
+ *   
  * Copyright (c) 2008, Dwoo / Jordi Boggiano
  * All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * 
  *  * Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
  *  * Redistributions in binary form must reproduce the above copyright
@@ -41,7 +41,7 @@
  *  * Neither the name of Dwoo nor the names of its contributors may be
  *    used to endorse or promote products derived from this software
  *    without specific prior written permission.
- *
+ *    
  * THIS SOFTWARE IS PROVIDED BY JORDI BOGGIANO ''AS IS'' AND ANY
  * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -52,40 +52,34 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  */
 
-if (defined("CAT_PATH")) {
-    include CAT_PATH . "/framework/class.secure.php";
+if (defined('CAT_PATH')) {	
+	include(CAT_PATH.'/framework/class.secure.php'); 
 } else {
-    $root = "../";
-    $level = 1;
-    while ($level < 10 && !file_exists($root . "/framework/class.secure.php")) {
-        $root .= "../";
-        $level += 1;
-    }
-    if (file_exists($root . "/framework/class.secure.php")) {
-        include $root . "/framework/class.secure.php";
-    } else {
-        trigger_error(
-            sprintf(
-                "[ <b>%s</b> ] Can't include class.secure.php!",
-                $_SERVER["SCRIPT_NAME"]
-            ),
-            E_USER_ERROR
-        );
-    }
+	$root = "../";
+	$level = 1;
+	while (($level < 10) && (!file_exists($root.'framework/class.secure.php'))) {
+		$root .= "../";
+		$level += 1;
+	}
+	if (file_exists($root.'framework/class.secure.php')) { 
+		include($root.'framework/class.secure.php'); 
+	} else {
+		trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+	}
 }
 
-$module_directory = "lib_dwoo";
-$module_name = "Dwoo Library for BlackCat CMS";
-$module_function = "library";
-$module_version = "0.14";
-$module_platform = "1.x";
-$module_author = "Dwoo/Jordi Boggiano, this module Black Cat Development";
-$module_license = "GNU General Public License";
-$module_description = "Dwoo PHP5 Template Engine, Release 1.1.1";
-$module_home = "http://dwoo.org/";
-$module_guid = "0266338D-DC1E-4815-B308-F9A6CB84448C";
+$module_directory     = 'lib_dwoo';
+$module_name          = 'Dwoo Library for BlackCat CMS';
+$module_function      = 'library';
+$module_version       = '0.13';
+$module_platform      = '1.x';
+$module_author        = 'Dwoo/Jordi Boggiano, this module Black Cat Development';
+$module_license       = 'GNU General Public License';
+$module_description   = 'Dwoo PHP5 Template Engine, Release 1.1.1';
+$module_home          = 'http://dwoo.org/';
+$module_guid          = '0266338D-DC1E-4815-B308-F9A6CB84448C';
 
 ?>
